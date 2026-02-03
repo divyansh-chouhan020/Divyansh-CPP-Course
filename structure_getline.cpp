814deb365e14cccfdf41203eb58f0b90978685f0
Divyansh chouhan <137995998+divyansh-chouhan020@users.noreply.github.com>
2026-02-03 10:46:48 +0530

    Structure with getline Array and string input handling

---
+++ structure_getline.cpp
@@ -0,0 +1,41 @@
+// -------------- How to take input in Array and string From ------------
+
+// getline : It takes line of inputs including your Spaces 
+// cin : It takes words only ignore text after the spaces 
+// ignore(n, '\n'): It ignore the next n buffer element ending with \name
+
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std  ;
+struct student{
+    int age ;
+    char name[20];
+    string book;
+} ;
+void PrintDetails(struct student s )
+{
+    cout <<"Age :- "<<s.age<<"\n"<<"Name :- "<<s.name<<"\n"<<"Book :- "<<s.book<<"\n";
+}
+int main()
+{
+    struct student s1={21, "Divyansh Chouhan", "Social Science"};
+    struct student s2 , s3;
+    
+    // manually enter the details 
+    s2.age =21;
+    strcpy(s2.name ,"Divyansh");
+    s2.book ="Hindi";
+    
+    // Taking input from user 
+    cout <<"Enter the Details Age , Name , Book : "<<endl ;
+    cin>>s3.age;
+    cin.ignore();
+    cin.getline(s3.name , 23);
+    getline(cin, s3.book);
+    
+    //  print all the details 
+    PrintDetails(s1);
+    PrintDetails(s2);
+    PrintDetails(s3);
+}
